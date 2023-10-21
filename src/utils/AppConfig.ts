9a0b5693912d42b86db8c557cacc22d07a2b59ee
jblanava <javiblanco2000@gmail.com>
2023-10-21 10:03:30 +0200

    studio template

--- src/utils/AppConfig.ts
+++
@@ -1,6 +0,0 @@
-export const AppConfig = {
-  site_name: 'Junction template',
-  title: 'Junction starter',
-  description: 'This is a template that will help us winning the junction 2023',
-  locale: 'en',
-};
